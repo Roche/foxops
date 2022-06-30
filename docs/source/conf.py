ff9143dcf6bbd4c3108e2531b16c85ec1d5cb45e
Timo Furrer <timo.furrer@roche.com>
2022-06-30 14:46:23 +0200

    Initial commit

---
+++ docs/source/conf.py
@@ -0,0 +1,71 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import sys
+from pathlib import Path
+
+DOCS_SOURCE_DIR = Path(__file__).parent
+SOURCE_DIR = DOCS_SOURCE_DIR.parent.parent / "src"
+
+sys.path.insert(0, str(SOURCE_DIR))
+
+
+# -- Project information -----------------------------------------------------
+
+project = "foxops"
+copyright = "2022, Timo Furrer"
+author = "Timo Furrer"
+
+# The full version, including alpha/beta/rc tags
+release = "v1"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions: list[str] = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.coverage",
+    "myst_parser",
+    "sphinx_design",
+    "sphinx_copybutton",
+    "sphinxcontrib.mermaid",
+]
+myst_enable_extensions = ["colon_fence"]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns: list[str] = []
+
+# Render autodoc by source
+autodoc_member_order = "bysource"
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "furo"
+html_title = "foxops"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path: list[str] = []
