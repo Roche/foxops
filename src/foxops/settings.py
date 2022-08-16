4086aa1bcabadc3d28ddf0a0720ebbbdb2cfbbcf
Timo Furrer <tuxtimo@gmail.com>
2022-08-16 11:56:03 +0200

    Make database url setting a secret string

--- src/foxops/settings.py
+++ src/foxops/settings.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     gitlab_address: str
     gitlab_token: SecretStr
     static_token: SecretStr
-    database_url: str = "sqlite+aiosqlite:///./test.db"
+    database_url: SecretStr = SecretStr("sqlite+aiosqlite:///./test.db")
     log_level: str = "INFO"
 
     class Config:
