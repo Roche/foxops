8e5348e5b633c396010e6d2ad9595bd981e90fa4
Umer <umer.hurramov@gmail.com>
2022-08-16 16:44:35 +0300

    Scaffolding

---
+++ ui/.eslintrc.cjs
@@ -0,0 +1,42 @@
+/* eslint-disable-next-line */
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'overrides': [
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'react/react-in-jsx-scope': 'off'
+  }
+}
---
+++ ui/src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div>hello foxops 🦊</div>
+  )
+}
+
+export default App
