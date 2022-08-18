64580f8b13290394ff19b965a06dbe9cbd1d694e
Umer <umer.hurramov@gmail.com>
2022-08-18 15:22:53 +0300

    Add localhost:3000 to CORS middleware

--- src/foxops/__main__.py
+++ src/foxops/__main__.py
@@ -32,7 +32,7 @@ def get_app():
     app.middleware("http")(request_middleware)
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost"],
+        allow_origins=["http://localhost:3000"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
