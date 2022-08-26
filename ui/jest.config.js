7d95092b557a0f90859ae9c1262ab86b2dd86153
Umer <umer.hurramov@gmail.com>
2022-08-26 10:37:22 +0300

    remove transform option from jest config

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -10,8 +10,5 @@ module.exports = {
   coverageDirectory: '<rootDir>/../coverage',
   moduleNameMapper: {
     '\\.(css|less|scss)$': '<rootDir>/support/style-mock.ts'
-  },
-  transform: {
-    '^.+\\.js$': 'babel-jest'
   }
 }
