fa81429b95ee99c9b319b8abda5e8d7adacbb3f3
Alexander Hungenberg <alexander.hungenberg@roche.com>
2022-09-04 23:47:27 +0200

    fix linting

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -1,7 +1,9 @@
-from pytest_alembic.tests import test_single_head_revision
-from pytest_alembic.tests import test_upgrade
-from pytest_alembic.tests import test_model_definitions_match_ddl
-from pytest_alembic.tests import test_up_down_consistency
+from pytest_alembic.tests import (
+    test_model_definitions_match_ddl,
+    test_single_head_revision,
+    test_up_down_consistency,
+    test_upgrade,
+)
 
 __all__ = [
     "test_single_head_revision",
