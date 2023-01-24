c59fa3a7939a2d4ff703cf05b94f4037be3055d7
FIRST_NAME LAST_NAME <pau.campana_soler@roche.com>
2023-01-24 13:19:41 +0100

    add get_next_ip function

--- tests/engine/test_rendering.py
+++ tests/engine/test_rendering.py
@@ -47,6 +47,7 @@ async def test_rendering_a_template_file_renders_data_in_file_content(tmp_path: 
     # THEN
     assert (incarnation_dir / "template.txt").read_text() == "Hello World"
 
+
 async def test_rendering_a_template_file_renders_data_in_file_content_using_custom_filter(tmp_path: Path):
     # GIVEN
     template_file = tmp_path / "template.txt"
@@ -67,6 +68,7 @@ async def test_rendering_a_template_file_renders_data_in_file_content_using_cust
 
     # THEN
     assert (incarnation_dir / "template.txt").read_text() == "1.2.3.5"
+
 
 async def test_rendering_a_template_file_with_invalid_templating_syntax_raises_exception(
     tmp_path: Path,
