db2d36a4fae02df57a9559c35ad1256bffae3b4c
Alexander Hungenberg <defreng@users.noreply.github.com>
2023-07-17 10:35:32 +0200

    add base64encoding (#368)
    * add base64encoding
    
    * fix lint

--- src/foxops/engine/custom_filters.py
+++ src/foxops/engine/custom_filters.py
@@ -1,3 +1,4 @@
+import base64
 import ipaddress
 
 from foxops.logger import get_logger
@@ -10,3 +11,12 @@ def ip_add_integer(ip: str, n: int = 1):
     """Add the integer n to the given IP address"""
     next_ip = ipaddress.ip_address(ip) + n
     return str(next_ip)
+
+
+def base64encode(data: str | bytes) -> str:
+    """Encode the given data as base64"""
+
+    if isinstance(data, str):
+        data = data.encode()
+
+    return base64.b64encode(data).decode()
