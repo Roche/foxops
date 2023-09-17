cf8d34eb21f91538e5211a3022a2f440f4d07e93
Alexander Hungenberg <alexander.hungenberg@roche.com>
2023-09-17 20:33:35 +0200

    fix

--- scripts/await-healthy.sh
+++ scripts/await-healthy.sh
@@ -9,7 +9,3 @@ done
 
 echo
 echo 'GitLab is healthy'
-
-# Print the version, since it is useful debugging information.
-curl --silent --show-error --header 'Authorization: Bearer ACCTEST1234567890123' http://${GITLAB_HOST:-127.0.0.1}:${GITLAB_PORT:-5002}/api/v4/version
-echo
