65f8c5fca99acb3b29e90b663c7d40435801f0b9
Alexander Hungenberg <alexander.hungenberg@roche.com>
2023-09-17 23:13:56 +0200

    fix

--- src/foxops/hosters/gitlab.py
+++ src/foxops/hosters/gitlab.py
@@ -155,7 +155,7 @@ class GitlabHoster(Hoster):
 
         # wait until Gitlab finished mergability checks
         for _ in range(5):
-            if merge_request["merge_status"] != "checking":
+            if merge_request["merge_status"] not in ["checking", "unchecked"]:
                 break
 
             response = await self.client.get(
